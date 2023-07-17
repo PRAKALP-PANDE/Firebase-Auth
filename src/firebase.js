24415005934f7d876492b96d761a00d58e780492
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-07-17 21:09:12 +0530

    create user in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -10,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,37 @@
-import "./login.scss"
+import { useState } from "react"
+import "./login.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
 
 const Login = () => {
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(true)
+      });
+
+  }
+
   return (
     <div className="login">
-      <form>
-        <input type="email" placeholder="Your Email" />
-        <input type="password" placeholder="Your password" />
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="Your Email" onChange={e=>setEmail(e.target.value)} />
+        <input type="password" placeholder="Your password" onChange={e=>setPassword(e.target.value)} />
         <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
       </form>
     </div>
   )
